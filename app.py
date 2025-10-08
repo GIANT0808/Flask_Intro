4f1700017ec8617c9332590974052d8b30c0c6fb
GIANTO808 <giancarlostumanan@gmail.com>
2025-10-09 04:17:18 +0800

    implemented css

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from flask import Flask, render_template, request
-<<<<<<< HEAD
 import math
 
 
@@ -7,27 +6,16 @@ app = Flask(__name__)
 
 
 #index
-=======
-
-app = Flask(__name__)
-
->>>>>>> 04210ba4083968f729d6821c95d2794bd9bbb7a1
 @app.route('/')
 def index():
     return render_template('index.html')
 
-<<<<<<< HEAD
 #profile
-=======
->>>>>>> 04210ba4083968f729d6821c95d2794bd9bbb7a1
 @app.route('/profile')
 def profile():
     return render_template('profile.html')
 
-<<<<<<< HEAD
 #works-STRING
-=======
->>>>>>> 04210ba4083968f729d6821c95d2794bd9bbb7a1
 @app.route('/works', methods=['GET', 'POST'])
 def works():
     result = None
@@ -36,7 +24,6 @@ def works():
         result = input_string.upper()
     return render_template('touppercase.html', result=result)
 
-<<<<<<< HEAD
 #works-CIRCLE area
 @app.route('/areaOfcircle', methods=['GET', 'POST'])
 def circle():
@@ -44,6 +31,7 @@ def circle():
         try:
             radius = float(request.form['radius'])
             area = math.pi * radius**2
+            formatted_area = round(area, 2)
             return render_template('CIRCLE.html', area=area, radius=radius)
         except ValueError:
             return render_template('CIRCLE.html', error="hehe, please enter a valid number. >.<")
@@ -57,6 +45,7 @@ def triangle():
             base = float(request.form['base'])
             height = float(request.form['height'])
             area = 0.5 * base * height
+            formatted_area = round(area, 2)
             return render_template('TRIANGLE.html', area=area, height=height, base=base)
         except ValueError:
             return render_template('TRIANGLE.html', error="hehe, please enter a valid number. >.<")
@@ -66,11 +55,5 @@ def triangle():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
-=======
-@app.route('/contact')
-def contact():
-    return "Contact Page. please create me an html page with dummy contact info"
-
->>>>>>> 04210ba4083968f729d6821c95d2794bd9bbb7a1
 if __name__ == "__main__":
     app.run(debug=True)
